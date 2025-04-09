63d6e114a68de62d2351329c59ba1f2ca0c86385
Dirk Arnez <smalldirkalex@gmail.com>
2025-04-09 19:58:55 +0800

    - add setup.cmd

--- local-init.cmd
+++ local-init.cmd
@@ -6,13 +6,6 @@ set PATH=^
 %JAVA_HOME%\bin;^
 %PREFIX%\gradle-8.13-bin\gradle-8.13\bin;
 
-if exist build (
-    echo.Deleting previous build...
-    rmdir /s /q build
-)
-
 java --version &&^
-.\gradlew.bat build &&^
-.\gradlew.bat assemble  &&^
-
+gradle.bat init
 pause
